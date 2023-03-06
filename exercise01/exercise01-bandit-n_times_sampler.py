65e50849ad9191e24d7bce094c55fb0e1d562862
Marc Scheffler <marcscheffler@outlook.com>
2023-03-06 13:24:56 +0100

    renamed n times sampler

--- exercise01/exercise01-bandit-n_times_sampler.py
+++ exercise01/exercise01-bandit-n_times_sampler.py
@@ -23,12 +23,14 @@ rewards_cum = [0] # custom
 actions = list(range(0, env.action_space.n))
 
 # idea: continuously sample arms (2 times each) and then progressively eliminate actions with lowest expected return
+n = 1 # how often should each arm be sampled between each elimination
+
 
 for i_episode in range(5000):
   
     print("episode Number is", i_episode)  
 
-    if len(actions) > 1 and (i_episode+1) % (len(actions)*2) == 0:
+    if len(actions) > 1 and (i_episode+1) % (len(actions)*n) == 0:
         remove = rewards_per_action.argmin()
         rewards_per_action = np.delete(rewards_per_action, remove)
         del actions[remove]
