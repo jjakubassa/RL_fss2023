fb1ac259dd37fd862c3e04a60791ec0529211005
Marc Scheffler <marcscheffler@outlook.com>
2023-03-08 17:24:51 +0100

    submission

--- exercise01/exercise01-bandit-n_times_halver_new.py
+++ exercise01/exercise01-bandit-n_times_halver_new.py
@@ -17,40 +17,46 @@ rewards = []
 average_rewards = np.zeros(env.action_space.n)
 nr_steps_per_action = np.zeros(env.action_space.n)
 
-rewards_per_action = np.zeros(env.action_space.n) # custom
-rewards_cum = [0] # custom
+### your code goes here ###
 
-actions = list(range(0, env.action_space.n))
-
-# idea: continuously sample arms (n times each) and then progressively eliminate actions with lowest expected return
+# idea: continuously sample arms (n times each) and then progressively eliminate actions with lowest expected return by halving
 n = 3 # how often should each arm be sampled between each halving of actions
+actions = list(range(0, env.action_space.n)) # all possible actions, in array
+rewards_per_action = np.zeros(env.action_space.n)
 
+###########################
 
 for i_episode in range(5000):
   
-    print("episode Number is", i_episode)  
-
-    if len(actions) > 1 and (i_episode+1) % (len(actions)*n) == 0:
-        floor = np.floor((len(actions)/2))
-        for x in range(int(floor)):
-            remove = rewards_per_action.argmin()
-            rewards_per_action = np.delete(rewards_per_action, remove)
-            del actions[remove]
-
+    print("episode Number is", i_episode)   
     
-    #action = env.action_space.sample() # sampling the "action" array which in this case only contains 10 "options" because there is 10 bandits
+    # action = env.action_space.sample() # sampling the "action" array which in this case only contains 10 "options" because there is 10 bandits
+    # action = i_episode % env.action_space.n
+    # print("action is", action)
+
+    ### your code goes here ###
+
+    # only remove actions when defined sampling amount has been reached and stop when only 1 action is left
+    if i_episode != 0 and len(actions) > 1: 
+        if i_episode % (len(actions)*n) == 0:
+            floor = np.floor(len(actions)/2) # amount of actions to be removed (half action amount, fractions get floored)
+            for x in range(int(floor)): # remove actions that yield lowest avg reward
+                remove = rewards_per_action.argmin()
+                rewards_per_action = np.delete(rewards_per_action, remove)
+                del actions[remove]
+
     action_index = i_episode % len(actions)
     action = actions[action_index]
     print("action is", action)
-
-
     ###########################
         
     # here we taking the next "step" in our environment by taking in our action variable randomly selected above
     observation, reward, done, info = env.step(action) 
-    rewards_per_action[action_index] += reward # custom
-    rewards_cum.append(rewards_cum[-1]+reward)
     rewards.append(reward)
+
+    ### your code goes here ###
+    rewards_per_action[action_index] += reward # used to eliminate action with lowest avg reward
+    ###########################
 
     print("observation space is: ",observation)
     print("reward variable is: ",reward)
@@ -58,10 +64,8 @@ for i_episode in range(5000):
     print("info variable is: ",info)
 
 print("sum of rewards: " + str(np.sum(rewards)))
-print("mean reward per action: " + str(np.sum(rewards)/i_episode))
 
-
-plt.plot(rewards_cum[1:])
+plt.plot(rewards)
 plt.ylabel('rewards')
 plt.xlabel('steps')
 plt.show()
